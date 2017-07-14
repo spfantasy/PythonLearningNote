debcc7b2fe6db50e6cad4868743810e17df33d88
李世豪 <shihao.li@baifendian.com>
2017-07-14 20:45:51 +0800

    PTB debugging

--- PTB/ptb_word_lm.py
+++ PTB/ptb_word_lm.py
@@ -1,12 +1,15 @@
 # -*- coding: UTF-8 -*-
 """
+#务必修改 vocab_size 大于等于 词典大小+1
+#务必修改 num_steps 小于等于 同一句长
+#务必修改 batch_size 为偶数
 The hyperparameters used in the model:
 - init_scale    - 权重初始化的方差
 - learning_rate - 初始学习速率
 - max_grad_norm - 梯度上限
 - num_layers    - LSTM 层数
-- num_steps     - LSTM 层数(时间维度)
-- hidden_size   - LSTM 单元数
+- num_steps     - LSTM 时间维展开长度
+- hidden_size   - LSTM 
 - max_epoch     - 当前学习速率学习的最大epoch数
 - max_max_epoch - 总epoch数
 - keep_prob     - 1-dropout几率
@@ -23,7 +26,7 @@ $ python ptb_word_lm.py --data_path=simple-examples/data/
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-#查看类初始化参数
+# 查看类初始化参数
 import inspect
 import time
 import sys
@@ -35,11 +38,11 @@ import reader
 
 flags = tf.flags
 logging = tf.logging
-#命令行参数
+# 命令行参数
 flags.DEFINE_string(
-    "model", "small",#选择运行的参数
+    "model", "small",  # 选择运行的参数
     "A type of model. Possible options are: small, medium, large.")
-flags.DEFINE_string("data_path", "../data/",#simple-examples/data/",
+flags.DEFINE_string("data_path", "../data/",  # simple-examples/data/",
                     "Where the training/test data is stored.")
 flags.DEFINE_string("save_path", "../model/",
                     "Model output directory.")
@@ -47,41 +50,37 @@ flags.DEFINE_bool("use_fp16", False,
                   "Train using 16-bit floats instead of 32bit floats")
 
 FLAGS = flags.FLAGS
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
 
 
 def data_type():
     return tf.float16 if FLAGS.use_fp16 else tf.float32
 
-
 class PTBInput(object):
-    #输入
-    def __init__(self, config, data, name=None):
+    # 输入
+
+    def __init__(self, config, data, label, name=None):
+        # 强制batch_size 为偶数
         self.batch_size = batch_size = config.batch_size
         self.num_steps = num_steps = config.num_steps
         self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
-        self.input_data, self.targets = reader.ptb_producer(
-            data, batch_size, num_steps, name=name)
-
+        self.input_data, self.labels = reader.ptb_producer(
+            data, label, batch_size, num_steps, name=name)
 
 class PTBModel(object):
-    #包裹Tensors的类
+    # 包裹Tensors的类
     #                   是否训练中   数据信息  数据流
+
     def __init__(self, is_training, config, input_):
         self._input = input_
-
         batch_size = input_.batch_size
         num_steps = input_.num_steps
         size = config.hidden_size
         vocab_size = config.vocab_size
 
-        # Slightly better results can be obtained with forget gate biases
-        # initialized to 1 but the hyperparameters of the model would need to be
-        # different than reported in the paper.
+        # forget_bias = 1.0结果稍好但需要调参
         def lstm_cell():
-            # With the latest TensorFlow source code (as of Mar 27, 2017),
-            # the BasicLSTMCell will need a reuse parameter which is unfortunately not
-            # defined in TensorFlow 1.0. To maintain backwards compatibility, we add
-            # an argument check here:
+            # 建立[1,hidden_size]的LSTM序列
             if 'reuse' in inspect.getargspec(
                     tf.contrib.rnn.BasicLSTMCell.__init__).args:
                 return tf.contrib.rnn.BasicLSTMCell(
@@ -91,17 +90,20 @@ class PTBModel(object):
                 return tf.contrib.rnn.BasicLSTMCell(
                     size, forget_bias=0.0, state_is_tuple=True)
         attn_cell = lstm_cell
+        # 需要dropout时用DropoutWrapper包裹序列
         if is_training and config.keep_prob < 1:
             def attn_cell():
                 return tf.contrib.rnn.DropoutWrapper(
                     lstm_cell(), output_keep_prob=config.keep_prob)
+        # cell为多个LSTM层堆叠的结构
         cell = tf.contrib.rnn.MultiRNNCell(
             [attn_cell() for _ in range(config.num_layers)], state_is_tuple=True)
-
+        # 初始化batch_size个cell的state
         self._initial_state = cell.zero_state(batch_size, data_type())
-
+        # 查询每个单词对应的向量
+        #[batch_size, num_steps]->[batch_size, num_steps, vocab_dim]
         with tf.device("/cpu:0"):
-            embedding = tf.get_variable(
+            self._embedding = embedding = tf.get_variable(
                 "embedding", [vocab_size, size], dtype=data_type())
             inputs = tf.nn.embedding_lookup(embedding, input_.input_data)
 
@@ -123,35 +125,40 @@ class PTBModel(object):
             for time_step in range(num_steps):
                 if time_step > 0:
                     tf.get_variable_scope().reuse_variables()
+                # 所有样本的第time_step个词输入到各自的cell里
                 (cell_output, state) = cell(inputs[:, time_step, :], state)
+                # outputs    - list of tensor[20,2]
                 outputs.append(cell_output)
-
-        output = tf.reshape(tf.stack(axis=1, values=outputs), [-1, size])
-        softmax_w = tf.get_variable(
-            "softmax_w", [size, vocab_size], dtype=data_type())
-        softmax_b = tf.get_variable(
-            "softmax_b", [vocab_size], dtype=data_type())
-        logits = tf.matmul(output, softmax_w) + softmax_b
-
-        # Reshape logits to be 3-D tensor for sequence loss
-        logits = tf.reshape(logits, [batch_size, num_steps, vocab_size])
-
-        # use the contrib sequence loss and average over the batches
-        loss = tf.contrib.seq2seq.sequence_loss(
-            logits,
-            input_.targets,
-            tf.ones([batch_size, num_steps], dtype=data_type()),
-            average_across_timesteps=False,
-            average_across_batch=True
-        )
-
+        # output  - Tensor[1,hidden_size(size)]
+        output = tf.reshape(tf.stack(axis=1, values=outputs), [-1,size])
+        fc1_w = tf.get_variable(
+            "fc1_w", [size, 1], dtype=data_type())
+        fc1_b = tf.get_variable(
+            "fc1_b", [1], dtype=data_type())
+        fc1 = tf.sigmoid(tf.matmul(output, fc1_w) + fc1_b)
+        fc1 = tf.reshape(fc1, [batch_size, num_steps* 1])
+        fc2_w = tf.get_variable(
+            "fc2_w", [num_steps* 1, 1], dtype=data_type())
+        fc2_b = tf.get_variable(
+            "fc2_b", [1], dtype=data_type()) 
+        _labels = tf.matmul(fc1, fc2_w) + fc2_b       
+        self._labels = _labels = tf.sigmoid(_labels)
+        # 使用input_.labels 和 logits计算 loss
+        loss = tf.square(_labels - tf.cast(input_.labels,data_type()))
+        #设定_labels的阈值
+        _labels_thres = tf.constant(np.full([_labels.shape[0].value,_labels.shape[1].value], .5),dtype=data_type())
+        # _labels二值化
+        prediction = tf.cast(tf.less(_labels_thres,_labels), tf.int32)
+        #计算准确度
+        correct_prediction = tf.equal(prediction,input_.labels)
+        self._accuracy = accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
         # update the cost variables
         self._cost = cost = tf.reduce_sum(loss)
         self._final_state = state
 
         if not is_training:
             return
-        #Tensor of Learning Rate
+        # Tensor of Learning Rate
         self._lr = tf.Variable(0.0, trainable=False)
         tvars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
@@ -173,12 +180,24 @@ class PTBModel(object):
         return self._input
 
     @property
+    def embedding(self):
+        return self._embedding
+
+    @property
     def initial_state(self):
         return self._initial_state
 
     @property
+    def labels(self):
+        return self._labels
+
+    @property
     def cost(self):
         return self._cost
+
+    @property
+    def accuracy(self):
+        return self._accuracy
 
     @property
     def final_state(self):
@@ -195,18 +214,18 @@ class PTBModel(object):
 
 class SmallConfig(object):
     """Small config."""
-    init_scale = 0.1
-    learning_rate = 1.0
+    init_scale = 1e-3#0.1
+    learning_rate = 0.1#1.0
     max_grad_norm = 5
     num_layers = 2
-    num_steps = 20
-    hidden_size = 200
+    num_steps = 80
+    hidden_size = 256
     max_epoch = 4
     max_max_epoch = 13
     keep_prob = 1.0
     lr_decay = 0.5
     batch_size = 20
-    vocab_size = 10000
+    vocab_size = 5000
 
 
 class MediumConfig(object):
@@ -215,14 +234,14 @@ class MediumConfig(object):
     learning_rate = 1.0
     max_grad_norm = 5
     num_layers = 2
-    num_steps = 35
+    num_steps = 80
     hidden_size = 650
     max_epoch = 6
     max_max_epoch = 39
     keep_prob = 0.5
     lr_decay = 0.8
     batch_size = 20
-    vocab_size = 10000
+    vocab_size = 5000
 
 
 class LargeConfig(object):
@@ -231,14 +250,14 @@ class LargeConfig(object):
     learning_rate = 1.0
     max_grad_norm = 10
     num_layers = 2
-    num_steps = 35
-    hidden_size = 1500
+    num_steps = 80
+    hidden_size = 256#1500
     max_epoch = 14
     max_max_epoch = 55
     keep_prob = 0.35
     lr_decay = 1 / 1.15
     batch_size = 20
-    vocab_size = 10000
+    vocab_size = 5000
 
 
 class TestConfig(object):
@@ -254,19 +273,21 @@ class TestConfig(object):
     keep_prob = 1.0
     lr_decay = 0.5
     batch_size = 20
-    vocab_size = 10000
+    vocab_size = 5000
 
 
 def run_epoch(session, model, eval_op=None, verbose=False):
     """Runs the model on the given data."""
     start_time = time.time()
     costs = 0.0
+    accuracys = 0.0
     iters = 0
     state = session.run(model.initial_state)
 
     fetches = {
         "cost": model.cost,
         "final_state": model.final_state,
+        "accuracy": model.accuracy,
     }
     if eval_op is not None:
         fetches["eval_op"] = eval_op
@@ -280,16 +301,20 @@ def run_epoch(session, model, eval_op=None, verbose=False):
         vals = session.run(fetches, feed_dict)
         cost = vals["cost"]
         state = vals["final_state"]
+        accuracy = vals["accuracy"]
 
         costs += cost
         iters += model.input.num_steps
+        accuracys += accuracy
 
         if verbose and step % (model.input.epoch_size // 10) == 10:
-            print("%.3f perplexity: %.3f speed: %.0f wps" %
-                  (step * 1.0 / model.input.epoch_size, np.exp(costs / iters),
+            print("%.3f error: %.3f accu: %.3f speed: %.0f wps" %
+                  (step * 1.0 / model.input.epoch_size, costs / step,
+                    accuracys/step,
                    iters * model.input.batch_size / (time.time() - start_time)))
 
-    return np.exp(costs / iters)
+    	print(session.run(model.embedding).reshape([-1,1])[:10])##
+    return accuracys/model.input.epoch_size
 
 
 def get_config():
@@ -304,28 +329,29 @@ def get_config():
     else:
         raise ValueError("Invalid model: %s", FLAGS.model)
 
-#张量流运行
+# 张量流运行
+
+
 def main(_):
     # 检查CMD或者文件头默认路径是否正确
     if not FLAGS.data_path:
         raise ValueError("Must set --data_path to PTB data directory")
 
     raw_data = reader.ptb_raw_data(FLAGS.data_path)
-    train_data, valid_data, test_data, _ = raw_data
-    #导入hyperparameter
+    train_data, valid_data, test_data, train_label_data, valid_label_data, test_label_data, _ = raw_data
+    # 导入hyperparameter
     config = get_config()
     eval_config = get_config()
-    eval_config.batch_size = 1
-    eval_config.num_steps = 1
+    # eval_config.batch_size = 1
 
     with tf.Graph().as_default():
-        #初始化LSTM参数
+        # 初始化LSTM参数
         initializer = tf.random_uniform_initializer(-config.init_scale,
                                                     config.init_scale)
 
         with tf.name_scope("Train"):
             train_input = PTBInput(
-                config=config, data=train_data, name="TrainInput")
+                config=config, data=train_data, label = train_label_data, name="TrainInput")
             with tf.variable_scope("Model", reuse=None, initializer=initializer):
                 m = PTBModel(is_training=True, config=config,
                              input_=train_input)
@@ -334,7 +360,7 @@ def main(_):
 
         with tf.name_scope("Valid"):
             valid_input = PTBInput(
-                config=config, data=valid_data, name="ValidInput")
+                config=config, data=valid_data, label = valid_label_data, name="ValidInput")
             with tf.variable_scope("Model", reuse=True, initializer=initializer):
                 mvalid = PTBModel(is_training=False,
                                   config=config, input_=valid_input)
@@ -342,34 +368,34 @@ def main(_):
 
         with tf.name_scope("Test"):
             test_input = PTBInput(config=eval_config,
-                                  data=test_data, name="TestInput")
+                                  data=test_data, label = test_label_data, name="TestInput")
             with tf.variable_scope("Model", reuse=True, initializer=initializer):
                 mtest = PTBModel(is_training=False, config=eval_config,
                                  input_=test_input)
-        #自动读取已有的checkpoint
-        #自带saver和summary模块，不需重复创建
+        # 自动读取已有的checkpoint
+        # 自带saver和summary模块，不需重复创建
         sv = tf.train.Supervisor(logdir=FLAGS.save_path)
-        with sv.managed_session() as session:
+        with sv.managed_session(config=tf.ConfigProto(gpu_options=gpu_options)) as session:
             for i in range(config.max_max_epoch):
                 lr_decay = config.lr_decay ** max(i +
                                                   1 - config.max_epoch, 0.0)
-                #对LearningRate进行下滑
+                # 对LearningRate进行下滑
                 m.assign_lr(session, config.learning_rate * lr_decay)
-                #读取当前Learning Rate
+                # 读取当前Learning Rate
                 print("Epoch: %d Learning rate: %.3f" %
                       (i + 1, session.run(m.lr)))
-                #进行一个epoch的训练
-                train_perplexity = run_epoch(session, m, eval_op=m.train_op,
-                                             verbose=True)
-                print("Epoch: %d Train Perplexity: %.3f" %
-                      (i + 1, train_perplexity))
-                #进行一个epoch的混乱度验证
-                valid_perplexity = run_epoch(session, mvalid)
-                print("Epoch: %d Valid Perplexity: %.3f" %
-                      (i + 1, valid_perplexity))
-            #测试集
-            test_perplexity = run_epoch(session, mtest)
-            print("Test Perplexity: %.3f" % test_perplexity)
+                # 进行一个epoch的训练
+                train_accuracy = run_epoch(session, m, eval_op=m.train_op,
+                                        verbose=True)
+                print("Epoch: %d Train accuracy: %.3f" %
+                      (i + 1, train_accuracy))
+                # 进行一个epoch的混乱度验证
+                valid_accuracy = run_epoch(session, mvalid)
+                print("Epoch: %d Valid accuracy: %.3f" %
+                      (i + 1, valid_accuracy))
+            # 测试集
+            test_accuracy = run_epoch(session, mtest)
+            print("Test accuracy: %.3f" % test_accuracy)
 
             if FLAGS.save_path:
                 print("Saving model to %s." % FLAGS.save_path)
--- PTB/reader.py
+++ PTB/reader.py
@@ -8,9 +8,9 @@ import collections
 import os
 import sys
 import pickle
+import numpy as np
 
 import tensorflow as tf
-
 
 def _read_words(filename):
     #从文件读取数据，以空格为切分符
@@ -25,9 +25,9 @@ def _read_words(filename):
         raise ValueError("_read_words : file not exist -",filename)
     with tf.gfile.GFile(filename, "r") as f:
         if sys.version_info[0] >= 3:
-            return f.read().replace("\n", "<eos>").split()
+            return f.read().replace("\n", " ").split()
         else:
-            return f.read().decode("utf-8").replace("\n", "<eos>").split()
+            return f.read().decode("utf-8").replace("\n", " ").split()
 
 
 def _build_vocab(filename):
@@ -97,6 +97,9 @@ def ptb_raw_data(data_path=None):
     train_path = os.path.join(data_path, "ptb.train.txt")
     valid_path = os.path.join(data_path, "ptb.valid.txt")
     test_path = os.path.join(data_path, "ptb.test.txt")
+    train_label_path = os.path.join(data_path, "ptb.train.label.txt")
+    valid_label_path = os.path.join(data_path, "ptb.valid.label.txt")
+    test_label_path = os.path.join(data_path, "ptb.test.label.txt")
 
     if os.path.exists(dict_path):
         word_to_id = _load_vocab(dict_path)
@@ -106,37 +109,73 @@ def ptb_raw_data(data_path=None):
     train_data = _file_to_word_ids(train_path, word_to_id)
     valid_data = _file_to_word_ids(valid_path, word_to_id)
     test_data = _file_to_word_ids(test_path, word_to_id)
+    train_label_data = [int(label) for label in _read_words(train_label_path)]
+    valid_label_data = [int(label) for label in _read_words(valid_label_path)]
+    test_label_data = [int(label) for label in _read_words(test_label_path)]
+    
     vocabulary = len(word_to_id)
-    return train_data, valid_data, test_data, vocabulary
+    return train_data, valid_data, test_data, train_label_data, valid_label_data, test_label_data, vocabulary
+
+# def chaos_generator(raw_data,vocab_size):
+#     # 将positive 的 句子随机混入指定程度的噪声，变为负样本
+#     # 0对应<eos>句尾标识符，vocab_size对应未知单词，均不随机生成
+#     # 调换，删减，添加，替换
+#     # Args:
+#     # raw_data      - 正确的样本
+#     # vocab_size    - 词典大小
+
+#     # Returns:
+#     #   list of integer
+#     chaos_data = raw_data
+#     last = 0
+#     for i in range(len(chaos_data)):
+#         if chaos_data[i] == 0 or i == len(raw_data) - 1:# 句尾
+#             if i<=1:
+#             idx = np.random.randint(last,i-1)
+#             chaos_data[idx] = np.random.randint(1,vocab_size)
+#             while(i < len(chaos_data) and chaos_data[i] == 0):
+#                 i += 1
+#                 last = i
+#     return chaos_data
 
 
-def ptb_producer(raw_data, batch_size, num_steps, name=None):
+def ptb_producer(raw_data, raw_label, batch_size, num_steps, name=None):
     # 从PTB数据迭代的读取batch大小的数据
     # 返回包含这些数据的Tensor
 
     # Args:
     #   raw_data    - ptb_raw_data 的返回值
     #               - list of integer
-    #   batch_size  - batch 大小
+    #   raw_label   - ptb_raw_data 的返回值(标签)
+    #               - list of integer
+    #   batch_size  - 每个batch包含的数据量
     #   num_steps   - LSTM 时间序列长度
     #   name: the name of this operation (optional).
 
     # Returns:
-    #   两组[batch_size, num_steps]大小的Tensor
-    #   第二个元素为左边相同数据时移+1
+    #   [batch_size, num_steps]大小的Tensor 奇数个为正样本，偶数个为负样本
+    #   数据的标签
 
     # Raises:
     # tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
-
     with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
         raw_data = tf.convert_to_tensor(
             raw_data, name="raw_data", dtype=tf.int32)
+        raw_label = tf.convert_to_tensor(
+            raw_label, name="raw_label", dtype=tf.int32)
         #总数据长度
         data_len = tf.size(raw_data)
-        #batch总数
-        batch_len = data_len // batch_size
+        #总句子个数
+        data_cols = data_len // num_steps
+        #batch大小
+        batch_len = data_cols // batch_size * num_steps
+        #1个epoch包含几个batch
+        epoch_size = (batch_len - 1) // num_steps
+
         data = tf.reshape(raw_data[0: batch_size * batch_len],
                           [batch_size, batch_len])
+        label = tf.reshape(raw_label[0: batch_size * epoch_size],
+                          [batch_size, epoch_size])
 
         epoch_size = (batch_len - 1) // num_steps
         assertion = tf.assert_positive(
@@ -146,14 +185,18 @@ def ptb_producer(raw_data, batch_size, num_steps, name=None):
             epoch_size = tf.identity(epoch_size, name="epoch_size")
 
         i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
+        #迭代的从[batch_size, batch_len]切割出[batch_size, num_steps]大小的完整句子的Tensor
         x = tf.strided_slice(data, [0, i * num_steps],
                              [batch_size, (i + 1) * num_steps])
         x.set_shape([batch_size, num_steps])
-        y = tf.strided_slice(data, [0, i * num_steps + 1],
-                             [batch_size, (i + 1) * num_steps + 1])
-        y.set_shape([batch_size, num_steps])
-        return x, y
+        #label
+        y = tf.strided_slice(label, [0, i],
+                             [batch_size, i + 1])
+        y.set_shape([batch_size])
+        return x,y
 
 if __name__ == "__main__":
-    train_data, valid_data, test_data, vocabulary = ptb_raw_data("../data")
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    all = ptb_raw_data("../data")
     print(1)
